91a1e0e16b98ad06088b181382237f5cdfee1d39
Nazar Ivato <nazarivato@gmail.com>
2016-04-18 17:57:55 +0300

    Continue improving tabs

--- www/js/controllers/missions.js
+++ www/js/controllers/missions.js
@@ -4,6 +4,13 @@
     var app = angular.module('DailyQuest');
 
     app.controller('MissionsController', ['$location', function($location){
+        this.tabs = [
+            {id: 'daily', label: 'Daily', content: 'Some Daily content'},
+            {id: 'weekly', label: 'Weekly', content: 'Some Weekly content'},
+            {id: 'monthly', label: 'Monthly', content: 'Some Monthly content'}
+        ];
+        this.activeTab = this.tabs[0];
+
     }]);
 
 })();
