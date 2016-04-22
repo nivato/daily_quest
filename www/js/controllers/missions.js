d4f815ce4b9bae234b6f9299d11e18e43207f14f
Nazar Ivato <nazarivato@gmail.com>
2016-04-22 17:21:32 +0300

    Improved swipe

--- www/js/controllers/missions.js
+++ www/js/controllers/missions.js
@@ -56,19 +56,52 @@
                 }
             });
 
+            var previousY;
             $('.scroll-container').swipe({
                 swipeStatus: function(event, phase, direction, distance, duration, fingerCount){
+                    if (previousY){
+                        if (Math.abs(event.y - previousY) < itemHeight / 4){
+                            return;
+                        }
+                        direction = (event.y - previousY > 0)? 'down': 'up';
+                    } else if (direction != 'up' && direction != 'down'){
+                        return;
+                    }
+                    previousY = event.y;
                     if (phase == 'move'){
-                        console.log(direction);
-                        if (!scrolling && (direction == 'up' || direction == 'down')){
+                        if (!scrolling){
                             var modifier = (direction == 'up')? -1: 1;
+
+                            if (new Date().getTime() - timestamp > 500){
+                                missedScrolls = 1;
+                            }
+                            var container = $(event.currentTarget);
+                            var list = container.parent();
+                            var moveDistance = itemHeight * missedScrolls * modifier;
+
+                            var listOffset = list.offset();
+                            var listHeight = list.outerHeight();
+
+                            var containerOffset = container.offset();
+                            var containerHeight = container.outerHeight();
+
+                            if (containerOffset.top + moveDistance > listOffset.top + 1){
+                                moveDistance = listOffset.top - containerOffset.top + 1;
+                            } else if (containerOffset.top + containerHeight + moveDistance < listOffset.top + listHeight - 1){
+                                moveDistance = (listOffset.top + listHeight - 1) - (containerOffset.top + containerHeight);
+                            }
+
                             $(event.currentTarget).animate(
-                                {top: '+=' + (itemHeight * modifier) + 'px'},
+                                {top: '+=' + moveDistance + 'px'},
                                 {
                                     duration: 100,
                                     start: function(){scrolling = true;},
                                     complete: function(){scrolling = false;}
                             });
+                            missedScrolls = 1;
+                            timestamp = new Date().getTime();
+                        } else {
+                            missedScrolls += 1;
                         }
                     }
                 },
